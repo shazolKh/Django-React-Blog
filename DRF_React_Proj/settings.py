6ce922a1b4a80d8a9fb7021e306301d8ee5af13b
shazolkh <shazol.kh@gmail.com>
2021-04-05 02:31:33 +0600

    Serializers added

--- DRF_React_Proj/settings.py
+++ DRF_React_Proj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 
     'blog',
     'blog_api',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from rest_framework import generics
 from blog.models import Post
-from .serializers import PostSerializers
+from .serializers import PostSerializer
 
 
 # Create your views here.
 
 
 class PostList(generics.ListCreateAPIView):
-    pass
+    queryset = Post.objects.filter(status='published').order_by('-published')
+    serializer_class = PostSerializer
 
 
 class PostDetails(generics.RetrieveDestroyAPIView):
-    pass
-
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
