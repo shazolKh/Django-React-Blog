fd44c963270fc5ae78517c53778c336c2884b5f1
shazolkh <shazol.kh@gmail.com>
2021-04-05 23:30:51 +0600

    DRF permissions added

--- DRF_React_Proj/urls.py
+++ DRF_React_Proj/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls'), name='blog'),
     path('api/', include('blog_api.urls'), name='blog_api'),
+    path('api-auth/', include('rest_framework.urls'), name='rest-auth'),
 ]
+
+# 17511055shazol
--- blog_api/views.py
+++ blog_api/views.py
@@ -2,16 +2,28 @@ from django.shortcuts import render
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import SAFE_METHODS, DjangoModelPermissions, BasePermission
 
 
 # Create your views here.
 
 
+class PostUserWritePermission(BasePermission):
+    message = 'Only author can edit the post..'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [DjangoModelPermissions]
     queryset = Post.objects.filter(status='published').order_by('-published')
     serializer_class = PostSerializer
 
 
-class PostDetails(generics.RetrieveDestroyAPIView):
+class PostDetails(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
